a0eb944bab671472e59f5c17902de37d28934993
hannahgmacca <hannahgmacca@gmail.com>
2021-11-30 11:54:40 +1000

    init

---
+++ src/Controller/ListController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function index(): Response
+    {
+
+        $companies = [
+            'Apple' => '$1.16 trillion USD',
+            'Samsung' => '$298.68 billion USD',
+            'Microsoft' => '$1.10 trillion USD',
+            'Alphabet' => '$878.48 billion USD',
+            'Intel Corporation' => '$245.82 billion USD',
+            'IBM' => '$120.03 billion USD',
+            'Facebook' => '$552.39 billion USD',
+            'Hon Hai Precision' => '$38.72 billion USD',
+            'Tencent' => '$3.02 trillion USD',
+            'Oracle' => '$180.54 billion USD',
+        ];
+
+        return $this->render('list/index.html.twig', [
+            'companies' => $companies,
+        ]);
+    }
+}
